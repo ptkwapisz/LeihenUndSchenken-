55f250b0d7f1afcf4aebd01617a07af22f5f231f
Piotr Kwapisz MacBook Pro <piotr_kwapisz@icloud.com>
2023-03-27 17:28:08 +0200

    Initial Commit

---
+++ LeihenSchenken/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  LeihenSchenken
+//
+//  Created by PIOTR KWAPISZ on 27.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LeihenSchenken/LeihenSchenkenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeihenSchenkenApp.swift
+//  LeihenSchenken
+//
+//  Created by PIOTR KWAPISZ on 27.03.23.
+//
+
+import SwiftUI
+
+@main
+struct LeihenSchenkenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
