e9a0704cb904de656781fa5d225e7a6ff443f427
Piotr Kwapisz Studio <piotr_kwapisz@icloud.com>
2023-09-13 21:40:50 +0200

    13.09.2023 - Xcode 15

--- LeihenSchenken/Pad/PadViews.swift
+++ LeihenSchenken/Pad/PadViews.swift
@@ -8,41 +8,58 @@
 import Foundation
 import SwiftUI
 
-
-
-/*
-
-
-struct TestTable: View {
+struct IpadTable2: View {
     @ObservedObject var globaleVariable = GlobaleVariable.shared
     
-    @State var personenTmp = querySQLAbfrageClassPersonen(queryTmp: "SELECT * FROM Personen")
-    @State var selectedItem: PersonClassVariable.ID?
-    @State private var sortOrder = [KeyPathComparator(\PersonClassVariable.personNachname)]
-    
+    @State var gegenstandeTmp = querySQLAbfrageClassGegenstaende(queryTmp: "SELECT * FROM Gegenstaende")
+    @State var selectedItem: GegenstandVariable.ID?
+ 
     var body: some View {
-    
+        
         
         VStack {
             Text("")
-            Text("Personen").bold()
+            Text("Gegenstände").bold()
             
-            Table(personenTmp, selection: $selectedItem, sortOrder: $sortOrder) {
+            Table(gegenstandeTmp, selection: $selectedItem) {
                 
-                //TableColumn("perKey", value: \.perKey)
-                TableColumn("Vorname", value: \.personVorname)
-                TableColumn("Nachname", value: \.personNachname)
-                TableColumn("Geschlecht", value: \.personSex)
+                TableColumn("perKey", value: \.perKey)
+                TableColumn("Gegenstand", value: \.gegenstandName)
                   
-           
             } // Ende Table
-            .onChange(of: sortOrder) {personenTmp.sort(using: $0)}
             .scrollContentBackground(.hidden)
             .listRowSeparatorTint(.gray)
-            .tableStyle(InsetTableStyle())
             .font(.custom("HelveticaNeue-Medium", size: 15))
-           
             
+            HStack(alignment: .bottom) {
+                Button(" + "){ // Aktion
+                            
+                }
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(Color.white)
+                
+                Text("|")
+                
+                Button(" - "){ // Aktion
+                            
+                }
+                .font(.system(size: 20, weight: .medium))
+                .foregroundColor(Color.white)
+                Text("|")
+                
+                Button(" Edit"){ // Aktion
+                            
+                }
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(Color.white)
+                
+            }
+            .frame(width: 395, height: 25, alignment: .leading)
+            .background(.gray)
+            .cornerRadius(10)
+            .foregroundColor(Color.black)
+            //.frame(maxWidth: .infinity, alignment: .leading)
+        
         } // Ende Vstack
         .background(globaleVariable.farbenEbene1)
         .cornerRadius(10)
@@ -50,9 +67,7 @@ struct TestTable: View {
     
         
     }// Ende var body
-   
+    
+    
     
 } // Ende struct
-
-
-*/
