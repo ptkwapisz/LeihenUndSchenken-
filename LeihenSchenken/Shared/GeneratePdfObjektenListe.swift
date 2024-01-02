0654e12a34a1e459a818f46f692147403abdf4ca
Piotr Kwapisz MacBook Pro <piotr_kwapisz@icloud.com>
2024-01-02 17:28:53 +0100

    2.01.2024 - Diverse vebesserungen

---
+++ LeihenSchenken/Shared/GeneratePdfObjektenListe.swift
@@ -0,0 +1,18 @@
+//
+//  GeneratePdfObjektenListe.swift
+//  LeihenSchenken
+//
+//  Created by PIOTR KWAPISZ on 26.12.23.
+//
+
+import SwiftUI
+
+struct GeneratePdfObjektenListe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GeneratePdfObjektenListe()
+}
---
+++ LeihenSchenken/Shared/ObjektenListeParameter.swift
@@ -0,0 +1,18 @@
+//
+//  ObjektenListeParameter.swift
+//  LeihenSchenken
+//
+//  Created by PIOTR KWAPISZ on 26.12.23.
+//
+
+import SwiftUI
+
+struct ObjektenListeParameter: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ObjektenListeParameter()
+}
