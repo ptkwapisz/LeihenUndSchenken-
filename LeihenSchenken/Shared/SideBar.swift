b3d471bac50b82ad0f4cc6cff96238534975e3f5
Piotr Kwapisz Studio <piotr_kwapisz@icloud.com>
2024-03-11 16:48:01 +0100

    11.03.2024 - Version 1.18 Leyout für iPad Mini, PDF-Liste, SideBar

---
+++ LeihenSchenken/Shared/SideBar.swift
@@ -0,0 +1,18 @@
+//
+//  SideBar.swift
+//  LeihenSchenken
+//
+//  Created by PIOTR KWAPISZ on 10.03.24.
+//
+
+import SwiftUI
+
+struct SideBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideBar()
+}
